18f5df538c009eccfad771acedc45b1bfa076b5e
Alex Beurthe <alexbeurthebasket1@orange.fr>
2020-10-15 13:42:42 +0200

    fin page calendrier + js page calendrier

---
+++ Js/script.js
@@ -0,0 +1,27 @@
+function calendrier_1() {
+  document.getElementById('day_1').style.visibility = 'unset';
+  document.getElementById('day_2').style.visibility = 'hidden';
+  document.getElementById('day_3').style.visibility = 'hidden';
+  document.getElementById('day_4').style.visibility = 'hidden';
+}
+
+function calendrier_2(){
+  document.getElementById('day_1').style.visibility = 'hidden';
+  document.getElementById('day_2').style.visibility = 'unset';
+  document.getElementById('day_3').style.visibility = 'hidden';
+  document.getElementById('day_4').style.visibility = 'hidden';
+}
+
+function calendrier_3(){
+  document.getElementById('day_1').style.visibility = 'hidden';
+  document.getElementById('day_2').style.visibility = 'hidden';
+  document.getElementById('day_3').style.visibility = 'unset';
+  document.getElementById('day_4').style.visibility = 'hidden';
+}
+
+function calendrier_4() {
+  document.getElementById('day_1').style.visibility = 'hidden';
+  document.getElementById('day_2').style.visibility = 'hidden';
+  document.getElementById('day_3').style.visibility = 'hidden';
+  document.getElementById('day_4').style.visibility = 'unset';
+}
